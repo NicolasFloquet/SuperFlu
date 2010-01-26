36cb1ecaa6ec5562f3a2905d516aaea75e546439
Addict7 <nicolasfloquet@gmail.com>
2010-01-26 09:06:13 +0000

    Update des flux finix

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Random;
+
 import entities.*;
 
 
@@ -6,6 +8,8 @@ import entities.*;
 public class GameLogic {
 	/*TODO: Calibrer TAUX_MIGRATION*/
 	private final static float TAUX_MIGRATION = 1000.0f; 
+	
+	private static Random rand; 
 	
 	private static GameLogic instance = null;
 	
@@ -15,6 +19,7 @@ public class GameLogic {
 	
 	
 	private GameLogic(){
+		rand = new Random();
 		joueurs = new ArrayList<Joueur>();
 		transferts = new ArrayList<Transfert>();	
 	}	
@@ -37,50 +42,65 @@ public class GameLogic {
 				
 				/* Pour chaque ville, on calcul le nombre d'habitants qui partent vers chaque ville */
 				float distance = 0.0f;
+				
 				int flux = 0;
+				int flux_sain = 0;
 				int flux_infecte = 0;
-				float taux_infection = ville_origine.getHabitants_infectes()/ville_origine.getHabitants();
-				float taux_immunisation = ville_origine.getHabitants_immunises()/ville_origine.getHabitants();
+				int flux_immunise = 0;
+				float taux_sain = ville_origine.getHabitantsSains()/ville_origine.getHabitants();
+				float taux_infection = ville_origine.getHabitantsInfectes()/ville_origine.getHabitants();
+				float taux_immunisation = ville_origine.getHabitantsImmunises()/ville_origine.getHabitants();
 				
 				for(Zone zone_dest : carte.getZones()){
 					for(Ville ville_dest : zone_dest.getVilles()){ /* Woot 4 boucles imbriquées */
 						distance = Ville.distance(ville_origine, ville_dest);
 						
 						/*Inserer ici une formule magique */
-						flux = (int) (TAUX_MIGRATION/distance); 
+						flux = (int) ((rand.nextGaussian()*TAUX_MIGRATION)/distance); 
 						
 						/*TODO: Trouver une meilleur formule pour le nombre de migrants infectés*/
+						flux_sain = (int)(taux_sain*flux);
 						flux_infecte = (int)(taux_infection*flux);
+						flux_immunise = (int)(taux_immunisation*flux);
 						 
-						/* Mise à jour de la population totale */
-						if(ville_origine.getHabitants()>=flux){
-							ville_dest.ajouteHabitants(flux);
-							ville_dest.retireHabitants(flux);
+						/* Mise à jour de la population saine*/
+						if(ville_origine.getHabitantsSains()>=flux_sain){
+							ville_dest.ajouteHabitantsSains(flux_sain);
+							ville_origine.retireHabitantsSains(flux_sain);
 						}else{
-							ville_dest.ajouteHabitants(ville_origine.getHabitants());
-							ville_dest.retireHabitants(ville.origine.getHabitants());
+							ville_dest.ajouteHabitantsSains(ville_origine.getHabitantsSains());
+							ville_origine.retireHabitantsSains(ville_origine.getHabitantsSains());
 						}
 						
-						/* Mise à jour de la population infectée */
-						if(ville_origine.getHabitants_infectes()>=flux_infecte){
-							ville_dest.setHabitants_infectes(ville_dest.getHabitants_infectes()+flux_infecte);
-							ville_origine.setHabitants_infectes(ville_origine.getHabitants_infectes()-flux_infecte);
+						/* Mise à jour de la population infectée*/
+						if(ville_origine.getHabitantsInfectes()>=flux_infecte){
+							ville_dest.ajouteHabitantsInfectes(flux_infecte);
+							ville_origine.retireHabitantsInfectes(flux_infecte);
 						}else{
-							ville_dest.setHabitants_infectes(ville_origine.getHabitants_infectes());
-							ville_origine.setHabitants_infectes(0);
+							ville_dest.ajouteHabitantsInfectes(ville_origine.getHabitantsInfectes());
+							ville_origine.retireHabitantsInfectes(ville_origine.getHabitantsInfectes());
 						}
-						
+												
 						/* Mise à jour de la population immunisée */
+						if(ville_origine.getHabitantsImmunises()>=flux_immunise){
+							ville_dest.ajouteHabitantsImmunises(flux_immunise);
+							ville_origine.retireHabitantsImmunises(flux_immunise);
+						}else{
+							ville_dest.ajouteHabitantsImmunises(ville_origine.getHabitantsImmunises());
+							ville_origine.retireHabitantsImmunises(ville_origine.getHabitantsImmunises());
+						}
 						
 					}
 				}
+				
+				/* Mise à jour de la ville */
+				ville_origine.update();
 				
 			}
 		}
 		
 		// Mise à jour des stocks
 		
-		// Mise à jour des infections
 		
 	}
 
